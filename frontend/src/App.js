12eceaf08e669612f8e724c00b3c67e6bd980a15
minul.alam <minul.alam@leads-bd.com>
2021-08-17 18:53:40 +0600

    frontend half done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import ProductScreen from "./screens/ProductScreen"
 
 const App = () => {
   return (
-    <>
-      <h1>Hello B</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Container, Nav, Navbar } from "react-bootstrap"
+import { LinkContainer } from "react-router-bootstrap"
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Leads Shop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
