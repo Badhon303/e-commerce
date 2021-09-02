c40a959452f82593c282f228b54806e5f78d8672
Badhon303 <badhon.alam303@gmail.com>
2021-09-03 01:24:28 +0600

    add to cart added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,42 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { Row, Col } from "react-bootstrap"
 import Product from "../components/Product"
-import axios from "axios"
+import { listProducts } from "../actions/productActions"
+import Loader from "../components/Loader"
+import Message from "../components/Message"
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products")
-      setProducts(data)
-    }
-    fetchProducts()
-  })
+    dispatch(listProducts())
+  }, [dispatch])
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger' children={error} />
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
 
 export default HomeScreen
+
+// 1. create store.js inlist reducer in combineReducers
+// 2. create exampleReducer.js (switch case action.type, action.payload, state)
+// 3. create exampleActions.js (thunk dispatch, type, payload)
+// 4. Implement { useDispatch, useSelector } in component
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,24 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
 
-const reducer = combineReducers({})
-const initialState = {}
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers.js"
+import { cartReducer } from "./reducers/cartReducers.js"
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+})
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : []
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+}
 const middleware = [thunk]
 const store = createStore(
   reducer,
@@ -12,3 +28,5 @@ const store = createStore(
 )
 
 export default store
+
+//npm i redux react-redux redux-thunk redux-devtools-extension
